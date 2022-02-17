bf14e77fdeb22292bae71096d1ceb766bea6241d
Harry <harry.tianhai@gmail.com>
2022-02-17 12:50:38 +0800

    gorm logger

--- go.mod
+++ go.mod
@@ -3,7 +3,12 @@ module github.com/HarryBird/mo-kit
 go 1.17
 
 require (
+	github.com/go-kratos/kratos/v2 v2.1.5
+	go.uber.org/zap v1.21.0
+	gorm.io/gorm v1.22.5
+)
+
+require (
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
-	go.uber.org/zap v1.21.0 // indirect
 )
--- log/kratos/gorm/gorm.go
+++ log/kratos/gorm/gorm.go
@@ -12,6 +12,13 @@ import (
 )
 
 const (
+	Silent = glog.Silent
+	Error  = glog.Error
+	Warn   = glog.Warn
+	Info   = glog.Info
+)
+
+const (
 	infoStr      = "%s\n[info] "
 	warnStr      = "%s\n[warn] "
 	errStr       = "%s\n[error] "
@@ -33,7 +40,7 @@ func New(l klog.Logger, config Config) glog.Interface {
 type logger struct {
 	Config
 
-	logger klog.Helper
+	logger *klog.Helper
 }
 
 // LogMode log mode
@@ -45,41 +52,41 @@ func (l *logger) LogMode(level glog.LogLevel) glog.Interface {
 
 // Info print info
 func (l logger) Info(ctx context.Context, msg string, data ...interface{}) {
-	if l.LogLevel >= glog.Info {
+	if l.LogLevel >= Info {
 		l.logger.Infof(infoStr+msg, append([]interface{}{utils.FileWithLineNum()}, data...)...)
 	}
 }
 
 // Warn print warn messages
 func (l logger) Warn(ctx context.Context, msg string, data ...interface{}) {
-	if l.LogLevel >= glog.Warn {
+	if l.LogLevel >= Warn {
 		l.logger.Warnf(warnStr+msg, append([]interface{}{utils.FileWithLineNum()}, data...)...)
 	}
 }
 
 // Error print error messages
 func (l logger) Error(ctx context.Context, msg string, data ...interface{}) {
-	if l.LogLevel >= glog.Error {
+	if l.LogLevel >= Error {
 		l.logger.Errorf(errStr+msg, append([]interface{}{utils.FileWithLineNum()}, data...)...)
 	}
 }
 
 // Trace print sql message
 func (l logger) Trace(ctx context.Context, begin time.Time, fc func() (string, int64), err error) {
-	if l.LogLevel <= glog.Silent {
+	if l.LogLevel <= Silent {
 		return
 	}
 
 	elapsed := time.Since(begin)
 	switch {
-	case err != nil && l.LogLevel >= glog.Error && (!errors.Is(err, glog.ErrRecordNotFound) || !l.IgnoreRecordNotFoundError):
+	case err != nil && l.LogLevel >= Error && (!errors.Is(err, glog.ErrRecordNotFound) || !l.IgnoreRecordNotFoundError):
 		sql, rows := fc()
 		if rows == -1 {
 			l.logger.Warnf(traceWarnStr, utils.FileWithLineNum(), err, float64(elapsed.Nanoseconds())/1e6, "-", sql)
 		} else {
 			l.logger.Warnf(traceWarnStr, utils.FileWithLineNum(), err, float64(elapsed.Nanoseconds())/1e6, rows, sql)
 		}
-	case elapsed > l.SlowThreshold && l.SlowThreshold != 0 && l.LogLevel >= glog.Warn:
+	case elapsed > l.SlowThreshold && l.SlowThreshold != 0 && l.LogLevel >= Warn:
 		sql, rows := fc()
 		slowLog := fmt.Sprintf("SLOW SQL >= %v", l.SlowThreshold)
 		if rows == -1 {
@@ -87,7 +94,7 @@ func (l logger) Trace(ctx context.Context, begin time.Time, fc func() (string, i
 		} else {
 			l.logger.Warnf(traceWarnStr, utils.FileWithLineNum(), slowLog, float64(elapsed.Nanoseconds())/1e6, rows, sql)
 		}
-	case l.LogLevel == glog.Info:
+	case l.LogLevel == Info:
 		sql, rows := fc()
 		if rows == -1 {
 			l.logger.Infof(traceStr, utils.FileWithLineNum(), float64(elapsed.Nanoseconds())/1e6, "-", sql)
